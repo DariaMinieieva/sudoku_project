10b11176b87116b9b5375b7ca7ea9f9582a22399
Alina Bondarets <alinium3012@gmail.com>
2021-05-26 22:32:44 +0300

    Add documentation

--- test_graph.py
+++ test_graph.py
@@ -1,10 +1,15 @@
+"""Module to test graph coloring from module graph.py."""
+
+
 import unittest
 from graph import Graph
 
 
 class TestGraphColoring(unittest.TestCase):
+    """Class to unit test class Graph from graph.py."""
 
     def setUp(self):
+        """Build initial graphs from edges lists."""
         self.graph_1 = Graph(
             [('A', 'B'), ('B', 'C'), ('A', 'C'), ('C', 'D'), ('A', 'D')])
         self.graph_2 = Graph([('A', 'B'), ('C', 'D')])
@@ -14,6 +19,10 @@ class TestGraphColoring(unittest.TestCase):
         self.graph_6 = Graph({('A', 'B'), ('A', 'C')})
 
     def test_assertion_errors(self):
+        """
+        Test exeptions (AssertionError) caught in function color_graph
+        with unsupported function arguments.
+        """
         with self.assertRaises(AssertionError) as err:
             self.graph_1.color_graph(0)
         self.assertEqual(str(err.exception),
@@ -28,8 +37,12 @@ class TestGraphColoring(unittest.TestCase):
             self.graph_1.color_graph([])
         self.assertEqual(str(err.exception),
                          'Colors number should be an integer.')
-    
+
     def test_incorrect_graph_file_representation(self):
+        """
+        Test function create_graph_from_file if the graph
+        representation format in the file is incorrect.
+        """
         graph = Graph()
 
         with self.assertRaises(AssertionError) as err:
@@ -38,6 +51,7 @@ class TestGraphColoring(unittest.TestCase):
                          'Vertices in the file should be represented as U: V')
 
     def test_connected_graph_and_read_from_file(self):
+        """Test connected graph."""
         self.assertEqual(self.graph_1.color_graph(2), None)
         self.assertEqual(self.graph_1.color_graph(3),
                          ['A:1', 'B:2', 'C:3', 'D:2'])
@@ -54,6 +68,7 @@ class TestGraphColoring(unittest.TestCase):
                          graph.color_graph(3))
 
     def test_disconnected_graph_and_read_from_file(self):
+        """Test disconnected graph."""
         self.assertEqual(self.graph_2.color_graph(1), None)
         self.assertEqual(self.graph_2.color_graph(2),
                          ['A:1', 'B:2', 'C:1', 'D:2'])
@@ -70,9 +85,11 @@ class TestGraphColoring(unittest.TestCase):
                          graph.color_graph(2))
 
     def test_single_vertex_graph(self):
+        """Test graph with only one vertex."""
         self.assertEqual(self.graph_3.color_graph(1), ['A:1'])
 
     def test_empty_vertices_list_and_read_file(self):
+        """Test graph built from an empty list and empty file."""
         self.assertEqual(self.graph_4.color_graph(1), [])
 
         graph = Graph()
@@ -81,9 +98,9 @@ class TestGraphColoring(unittest.TestCase):
 
         self.assertEqual(self.graph_4.color_graph(1),
                          graph.color_graph(1))
-        
 
     def test_graph_built_from_set(self):
+        """Test graph built from a set of edges."""
         self.assertEqual(self.graph_5.color_graph(1), [])
         self.assertTrue(set(self.graph_6.color_graph(2)) in (
             {'A:1', 'B:2', 'C:2'}, {'A:2', 'B:1', 'C:1'}))
